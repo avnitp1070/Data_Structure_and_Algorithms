ca013b2313ffab612e8fa9855762ea2c3225c6c2
Avnit Prakash <103447988+avnitp1070@users.noreply.github.com>
2023-06-12 16:51:16 +0530

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.cpp
+++ Number Of Enclaves - GFG/number-of-enclaves.cpp
@@ -10,17 +10,25 @@ using namespace std;
 
 class Solution {
   public:
-    void dfs(int i, int j,int n, int m, vector<vector<int>> &vis,vector<vector<int>> &grid){
-        int dx[4]={-1,0,1,0};
+    int dx[4]={-1,0,1,0};
     int dy[4]={0,1,0,-1};
+    void bfs(int i, int j,int n, int m, vector<vector<int>> &vis,vector<vector<int>> &grid){
         vis[i][j]=1;
-        for(int k=0;k<4;k++){
-            int x=i+dx[k];
-            int y=j+dy[k];
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        while(!q.empty()){
+            auto p=q.front();q.pop();
+            int a=p.first;
+            int b=p.second;
+            for(int k=0;k<4;k++){
+            int x=a+dx[k];
+            int y=b+dy[k];
             if(x<0 || x>=n || y<0 || y>=m || vis[x][y]==1 || grid[x][y]==0){
             	continue;
             	}
-            dfs(x,y,n,m,vis,grid);
+            vis[x][y]=1;
+            q.push({x,y});
+        }
         }
     }
     
@@ -34,7 +42,7 @@ class Solution {
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1 && vis[i][j]==0){
                   if(i==0 || i==n-1 || j==0 || j==m-1 )
-                  dfs(i,j,n,m,vis,grid);
+                  bfs(i,j,n,m,vis,grid);
                 }
             }
         }
