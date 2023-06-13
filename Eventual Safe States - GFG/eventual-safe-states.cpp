4064845bb38ac26229c068b20202a401383fdb7e
Avnit Prakash <103447988+avnitp1070@users.noreply.github.com>
2023-06-13 16:38:48 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -10,35 +10,81 @@ using namespace std;
 
 class Solution {
   public:
-  bool dfs(int node,vector<int> adj[],int vis[]){
-        vis[node]=1;
-        for(auto &it:adj[node]){
-            if(vis[it]==0){
-              if(dfs(it,adj,vis)==true){
-                  return true;
-             }
+//   bool dfs(int node,vector<int> adj[],int vis[]){
+//         vis[node]=1;
+//         for(auto &it:adj[node]){
+//             if(vis[it]==0){
+//               if(dfs(it,adj,vis)==true){
+//                   return true;
+//              }
+//             }
+//              else if(vis[it]==1){
+//                  return true;
+//              }
+//         }
+//         vis[node]=2; // mark as safe node
+//         return false;
+//     }
+//     vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+//         // code here
+//         int vis[V]={0};
+//         vector<int>ans;
+//         for(int i=0;i<V;i++){
+//             if(!vis[i]){
+//                 dfs(i,adj,vis);
+//             }
+//         }
+//         for(int i=0;i<V;i++){
+//             if(vis[i]==2)
+//               ans.push_back(i);
+//         }
+//         return ans;
+//     }
+    
+    vector<int>topo(int n,vector<int>adj[]){
+        queue<int>q;
+        vector<int>vis(n,0);
+        vector<int> tr[n];
+        int outdeg[n]={0};
+        for(int i=0;i<n;i++){
+            for(auto x:adj[i]){
+             outdeg[i]++;
+             vis[i]=1;
+             tr[x].push_back(i);
             }
-             else if(vis[it]==1){
-                 return true;
-             }
         }
-        vis[node]=2; // mark as safe node
-        return false;
+        
+        for(int i=0;i<n;i++){
+            if(outdeg[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>ans;
+        while(!q.empty()){
+            int x=q.front();
+            q.pop();
+            ans.push_back(x);
+            for(auto child:tr[x]){
+                outdeg[child]--;
+                if(outdeg[child]==0){
+                    q.push(child);
+                }
+            }
+        }
+        
+        // for(int i=0;i<n;i++){
+        //     if(vis[i]==1){
+        //         ans.push_back(i);
+        //     }
+        // 
+        
+        //}
+        sort(ans.begin(),ans.end());
+        return ans;
     }
     vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
         // code here
-        int vis[V]={0};
-        vector<int>ans;
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                dfs(i,adj,vis);
-            }
-        }
-        for(int i=0;i<V;i++){
-            if(vis[i]==2)
-              ans.push_back(i);
-        }
-        return ans;
+        return topo(V,adj);
     }
 };
 
