3f0f581f2683451ec8aa72ded62e47f39abcd83d
Avnit Prakash <103447988+avnitp1070@users.noreply.github.com>
2023-06-23 12:14:25 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -14,10 +14,10 @@ class Solution {
 	   int n=matrix.size();
 	   for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (matrix[i][j] == -1) {
+				if (matrix[i][j]==-1) {
 					matrix[i][j] = 1e9;
 				}
-				if (i == j) matrix[i][j] = 0;
+				if (i == j) matrix[i][j]=0;
 			}
 		}
 	    
@@ -32,28 +32,12 @@ class Solution {
 	    
 	    for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (matrix[i][j] == 1e9) {
-					matrix[i][j] = -1;
+				if (matrix[i][j]==1e9) {
+					matrix[i][j]=-1;
 				}
 			}
 		}
 	   
-	   //int n = matrix.size();
-		
-
-// 		for (int k = 0; k < n; k++) {
-// 			for (int i = 0; i < n; i++) {
-// 				for (int j = 0; j < n; j++) {
-// 					matrix[i][j] = min(matrix[i][j],
-// 					                   matrix[i][k] + matrix[k][j]);
-// 				}
-// 			}
-// 		}
-
-
-
-
-// 		
 	}
 };
 
