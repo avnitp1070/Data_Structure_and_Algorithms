b3c7ebc85d24b28a7be823a91c3feeede6a09f4e
Avnit Prakash <103447988+avnitp1070@users.noreply.github.com>
2023-06-23 14:37:47 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -8,32 +8,53 @@ using namespace std;
 class Solution
 {
 	public:
+	
+	vector<int>parent;
+ void make_set(int n){
+    parent.resize(n+5); 
+    for(int i=0;i<n+5;i++){
+     parent[i]=i;
+ }
+}
+int find(int x){
+       if( parent[x] == x) {
+            return x;
+        }
+       return parent[x]=find(parent[x]);
+    }
+
+bool union_set(int x,int y){
+        int p=find(x);
+        int q=find(y);
+        if(p==q){
+            return true;
+        }
+        parent[p]=q;
+        return false;
+    }
+    
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int n, vector<vector<int>> adj[])
     {
         // code here
-        vector<int>vis(n+1,0);
+        vector<pair<int,pair<int,int>>>v;
         int sum=0;
-        priority_queue<pii,vector<pii>,greater<pii>>pq;
-        pq.push({0,0});
-        while(!pq.empty()){
-            int node=pq.top().second;
-            int wt=pq.top().first;
-            pq.pop();
-            if(vis[node]==1){
+        for(int i=0;i<n;i++){
+            for(auto x:adj[i]){
+                v.push_back({x[1],{i,x[0]}});
+            }
+        }
+        make_set(n);
+        sort(v.begin(),v.end());
+        
+        for(auto x:v){
+            int a=x.second.first;
+            int b=x.second.second;
+            if(find(a)==find(b)){
                 continue;
             }
-            vis[node]=1;
-            sum+=wt;
-            
-            for(auto it:adj[node]){
-                int child=it[0];
-                int adj_wt=it[1];
-                if(vis[child]==0){
-                    pq.push({adj_wt,child});
-                }
-                
-            }
+            sum+=x.first;
+            union_set(a,b);
         }
         return sum;
     }
