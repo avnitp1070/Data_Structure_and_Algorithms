646c72b53793368b34554e605f2f0eaad0de0112
Avnit Prakash <103447988+avnitp1070@users.noreply.github.com>
2023-06-23 12:10:48 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -11,17 +11,15 @@ class Solution {
   public:
 	void shortest_distance(vector<vector<int>>&matrix){
 	    // Code here
-	   // int n=matrix.size();
-	   // for(int i=0;i<n;i++){
-	   //     for(int j=0;j<n;j++){
-	   //         if(matrix[i][j]==-1){
-	   //             matrix[i][j]==1e9;
-	   //         }
-	   //         if(i==j){
-	   //             matrix[i][j]=0;
-	   //         }
-	   //     }
-	   // }
+	   int n=matrix.size();
+	   for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (matrix[i][j] == -1) {
+					matrix[i][j] = 1e9;
+				}
+				if (i == j) matrix[i][j] = 0;
+			}
+		}
 	    
 	   // for(int k=0;k<n;k++){
 	   //    for(int i=0;i<n;i++){
@@ -40,15 +38,8 @@ class Solution {
 	   //     }
 	   // }
 	   
-	   int n = matrix.size();
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (matrix[i][j] == -1) {
-					matrix[i][j] = 1e9;
-				}
-				if (i == j) matrix[i][j] = 0;
-			}
-		}
+	   //int n = matrix.size();
+		
 
 		for (int k = 0; k < n; k++) {
 			for (int i = 0; i < n; i++) {
