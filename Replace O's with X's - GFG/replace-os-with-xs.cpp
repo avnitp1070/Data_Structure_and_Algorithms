b523bcddce2de1476908ff5b10688cdeb41972b0
Avnit Prakash <103447988+avnitp1070@users.noreply.github.com>
2023-09-04 09:31:28 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.cpp
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -9,43 +9,48 @@ using namespace std;
 
 class Solution{
 public:
-      int dx[4]={-1,0,1,0};
-     int dy[4]={0,1,0,-1};
-    void dfs(int i, int j,int n, int m, vector<vector<int>> &vis,vector<vector<char>> &mat){
-        vis[i][j]=1;
-        for(int k=0;k<4;k++){
-            int x=i+dx[k];
-            int y=j+dy[k];
-            if(x<0 || x>=n || y<0 || y>=m || vis[x][y]==1 || mat[x][y]=='X'){
-            	continue;
-            	}
-            dfs(x,y,n,m,vis,mat);
+    int dx[4]={1,0,-1,0};
+    int dy[4]={0,1,0,-1};
+    bool isValid(int x,int y,int n,int m){
+        if(x>=n || x<0 || y>=m || y<0){
+            return 0;
+        }
+        return 1;
+    }
+    void dfs(int x,int y,vector<vector<char>> &mat,vector<vector<int>> &vis){
+        vis[x][y]=1;
+        for(int i=0;i<4;i++){
+            int nx=x+dx[i];
+            int ny=y+dy[i];
+            if(isValid(nx,ny,mat.size(),mat[0].size()) && vis[nx][ny]==0 && mat[nx][ny]=='O'){
+                dfs(nx,ny,mat,vis);
+            }
         }
     }
     vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
     {
         // code here
-         
-    vector<vector<int>>vis(n,vector<int>(m,0));
+        vector<vector<int>> vis(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(mat[i][j]=='O' && vis[i][j]==0){
-                  if(i==0 || i==n-1 || j==0 || j==m-1 )
-                  dfs(i,j,n,m,vis,mat);
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    if(mat[i][j]=='O')
+                      dfs(i,j,mat,vis);
                 }
             }
         }
-        vector<vector<char>>ans(n,vector<char>(m));
+        vector<vector<char>> ans=mat;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis[i][j]==0){
-                  ans[i][j]='X';
-                }
-                else{
+                if(vis[i][j]){
                     ans[i][j]='O';
                 }
+                else{
+                    ans[i][j]='X';
+                }
             }
         }
+        
         return ans;
     }
 };
