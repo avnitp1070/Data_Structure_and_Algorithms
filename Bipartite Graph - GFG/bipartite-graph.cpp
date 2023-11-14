c7b6bd12580c255f23d218c972c9d54d1feb2503
Avnit Prakash <103447988+avnitp1070@users.noreply.github.com>
2023-11-14 12:27:53 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -6,30 +6,35 @@ using namespace std;
 class Solution {
 public:
 
-    bool dfs(int x,int c,vector<int>&vis,vector<int>&col,vector<int>adj[]){
-        vis[x]=1;
-        col[x]=c;
-        for(auto child:adj[x]){
-            if(vis[child]==0){
-                if(dfs(child,c^1,vis,col,adj)==0){
-                    return 0;
-                }
-            }
-            else{
-                if(col[x]==col[child]){
-                    return 0;
-                }
-            }
-        }
-        return 1;
-        
+    bool bfs(int sx,int c,vector<int>&vis,vector<int>&col,vector<int>adj[]){
+       queue<pair<int,int>>q;
+       q.push({sx,c});
+       vis[sx]=1;
+       col[sx]=c;
+       while(!q.empty()){
+           auto p=q.front();q.pop();
+           int x=p.first;
+           for(auto child:adj[x]){
+               if(vis[child]==0){
+                   q.push({child,col[x]^1});
+                   col[child]=col[x]^1;
+                   vis[child]=1;
+               }
+               else {
+                   if(col[child]==col[x]){
+                       return 0;
+                   }
+               }
+           }
+       }
+       return 1;
     }
 	bool isBipartite(int n, vector<int>adj[]){
 	    // Code here
 	     vector<int>vis(n+1,0),col(n+1,0);
 	     for(int i=0;i<n;i++){
 	         if(vis[i]==0){
-	             if(dfs(i,0,vis,col,adj)==0){
+	             if(bfs(i,0,vis,col,adj)==0){
 	                 return 0;
 	             }
 	         }
